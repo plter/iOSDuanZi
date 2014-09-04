4bcc58b5887af2ba37f1d1a67696de8bfd30ee4f
ime@极客学院 <xtiqin@163.com>
2014-09-04 17:02:36 +0800

    创建工程
    第一次提交代码

---
+++ Duanzi/Duanzi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Duanzi
+//
+//  Created by plter on 9/4/14.
+//  Copyright (c) 2014 plter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Duanzi/Duanzi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Duanzi
+//
+//  Created by plter on 9/4/14.
+//  Copyright (c) 2014 plter. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
